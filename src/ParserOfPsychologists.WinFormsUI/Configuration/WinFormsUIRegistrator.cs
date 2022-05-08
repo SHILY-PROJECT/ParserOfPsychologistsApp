8271f4cdf4a1e1d0e4c11b81f0fcea4c9ff2536a
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-08 13:09:54 +0300

    -Добавлены юзинги -Добавлен класс с регистрацией сервисов DI

---
+++ src/ParserOfPsychologists.WinFormsUI/Configuration/WinFormsUIRegistrator.cs
@@ -0,0 +1,13 @@
+﻿namespace ParserOfPsychologists.WinFormsUI.Configuration;
+
+public static class WinFormsUIRegistrator
+{
+    public static IServiceCollection AddWinFormsUI(this IServiceCollection services)
+    {
+        services
+            .AddSingleton<MainForms>()
+            .AddApplication();
+
+        return services;
+    }
+}
