f5efff40ec002cf66d9b21d106a246dfd2d6cba7
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-08 13:52:03 +0300

    -Добавлены юзинги и регер сервисов

---
+++ src/ParserOfPsychologists.ConsoleUI/Configuration/ConsoleUIRegistrator.cs
@@ -0,0 +1,14 @@
+﻿namespace ParserOfPsychologists.ConsoleUI.Configuration;
+
+public static class ConsoleUIRegistrator
+{
+    public static IServiceCollection AddConsoleUI(this IServiceCollection services)
+    {
+        services
+            .AddApplication()
+            .AddSingleton<Startup>()
+            .AddScoped<AccountData>();
+
+        return services;
+    }
+}
---
+++ src/ParserOfPsychologists.ConsoleUI/Configuration/_usings.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using ParserOfPsychologists.Application.Models;
+global using ParserOfPsychologists.Application.Interfaces;
+global using ParserOfPsychologists.Application.Configuration;
+global using ParserOfPsychologists.ConsoleUI.Configuration;
