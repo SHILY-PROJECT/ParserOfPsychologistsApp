8e32c2d7ded4185225cfdc6a8c2b1c6b567955cb
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-08 13:51:19 +0300

    -Добавлен регер и стартап для UI

---
+++ src/ParserOfPsychologists.ConsoleUI/Startup.cs
@@ -0,0 +1,25 @@
+﻿namespace ParserOfPsychologists.ConsoleUI;
+
+internal class Startup
+{
+    private readonly IParserWebRequestsFacade _webRequestsFacade;
+
+    public Startup(IParserWebRequestsFacade webRequestsFacade)
+    {
+        _webRequestsFacade = webRequestsFacade;
+    }
+    
+    public async Task Run()
+    {
+        var resp = await _webRequestsFacade.ParseUsersByCityAsync(new Uri("https://www.b17.ru/psiholog/moskva/"));
+        Console.WriteLine();
+        Console.ReadKey();
+    }
+
+    internal static async Task Main(string[] args) =>
+        await CreateHostBuilder(args).Build().Services.GetRequiredService<Startup>().Run();
+
+    private static IHostBuilder CreateHostBuilder(string[] args) => Host
+        .CreateDefaultBuilder(args)
+        .ConfigureServices(services => services.AddConsoleUI());
+}
