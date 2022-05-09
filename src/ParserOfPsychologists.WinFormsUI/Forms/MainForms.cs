ab989852576b10621ab63ae05946a3fc7ee4fe76
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-10 00:04:23 +0300

    -Добавлена подписка на событие выбора значения значения из списка

--- src/ParserOfPsychologists.WinFormsUI/Forms/MainForms.cs
+++ src/ParserOfPsychologists.WinFormsUI/Forms/MainForms.cs
@@ -17,11 +17,20 @@ public partial class MainForms : Form
     private void RegisterFormEvents()
     {
         ActionOnEventsToLoadAndCloseForm();
+        ActionOnEventsChangeParserSettings();
     }
 
     private void ActionOnEventsToLoadAndCloseForm()
     {
         var cities = async () => (await _facade.FindCityAsync(string.Empty)).ToArray();
         this.Load += async (s, e) => this.citiesBox.Items.AddRange(await cities.Invoke());
+    }
+
+    private void ActionOnEventsChangeParserSettings()
+    {
+        this.citiesBox.SelectedValueChanged += async (s, e) =>
+        {
+            if (s is ComboBox box && _stateOfCity.IsChanged(box.Text)) await _stateOfCity.ChangeCityAsync(box.Text);
+        };
     }
 }
