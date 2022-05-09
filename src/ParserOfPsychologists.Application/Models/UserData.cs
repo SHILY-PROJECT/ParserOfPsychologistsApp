4571c3600dfd57328ff438d7ab1199a29de720a7
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-09 20:33:31 +0300

    -Добавлена модель пользователя

---
+++ src/ParserOfPsychologists.Application/Models/UserData.cs
@@ -0,0 +1,21 @@
+﻿namespace ParserOfPsychologists.Application.Models;
+
+public record UserData
+{
+    public UserData() { }
+    public UserData(string fullName) => FullName = fullName;
+    public UserData(string fullName, Uri userUrl) => (FullName, UrlOnSite) = (fullName, userUrl.OriginalString);
+
+    public string FullName { get; set; } = string.Empty;
+    public string UrlOnSite { get; set; } = string.Empty;
+    public string Specialty { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public UserContactsData? Contacts { get; set; }
+
+    public void ExtractSpecialtyAndCity(string specialtyAndCity)
+    {
+        var sc = specialtyAndCity.Replace("&nbsp;", "").Split('–');
+        Specialty = sc.FirstOrDefault() ?? string.Empty;
+        City = sc.LastOrDefault() ?? string.Empty;
+    }
+}
