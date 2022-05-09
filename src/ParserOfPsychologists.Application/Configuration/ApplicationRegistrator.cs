e19b2c341ddd8af6d842b97df041f747ec36a9cd
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-09 20:47:02 +0300

    -Добавлен фасад для приложения -Добавлен класс с регистрацией сервисов

---
+++ src/ParserOfPsychologists.Application/Configuration/ApplicationRegistrator.cs
@@ -0,0 +1,21 @@
+﻿namespace ParserOfPsychologists.Application.Configuration;
+
+public static class ApplicationRegistrator
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var cfg = HttpClientConfiguration.CreateConfiguration();
+
+        services
+            .AddScoped<IStateOfCityModule, StateOfCityModule>()
+            .AddScoped<IParser, MainParser>()
+            .AddScoped<IParserWebRequestsFacade, ParserWebFacade>()
+            .AddScoped<IHttpClientConfiguration, HttpClientConfiguration>(opt => cfg)
+            .AddScoped<IAccountManager, AccountManager>()
+            .AddScoped<AccountData>()
+            .AddScoped<AuthorizationModule>()
+            .AddTransient(opt => HttpHelper.CreateHttpClient(cfg));
+
+        return services;
+    }
+}
---
+++ src/ParserOfPsychologists.Application/ParserWebFacade.cs
@@ -0,0 +1,38 @@
+﻿namespace ParserOfPsychologists.Application;
+
+public class ParserWebFacade : IParserWebRequestsFacade
+{
+    private readonly IParser _parser;
+    private readonly IStateOfCityModule _cityModule;
+    private readonly IAccountManager _accountManager;
+
+    public ParserWebFacade(IParser parser, IStateOfCityModule cityModule, IAccountManager accountManager)
+    {
+        _parser = parser;
+        _cityModule = cityModule;
+        _accountManager = accountManager;
+    }
+
+    public async Task<bool> ConnectAnAccountAsync()
+    {
+        try
+        {
+            await _accountManager.ConnectAnAccountAsync();
+        }
+        catch
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public async Task<IReadOnlyCollection<string>> FindCityAsync(string cityName)
+    {
+        return (await _cityModule.FindCityAsync(cityName)).ToArray();
+    }
+
+    public async Task<IReadOnlyCollection<UserData>> ParseUsersByCityAsync()
+    {
+        return (await _parser.ParseUsersByCityAsync()).ToList();
+    }
+}
