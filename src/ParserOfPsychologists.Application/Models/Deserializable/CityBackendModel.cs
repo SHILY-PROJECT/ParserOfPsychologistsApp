9e412dffe98962af50c701d011cb91428cf427f4
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-09 20:30:48 +0300

    -Добавлены модельки для десериализации ответов с бэка

---
+++ src/ParserOfPsychologists.Application/Models/Deserializable/CityBackendModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ParserOfPsychologists.Application.Models.Deserializable;
+
+public class CityBackendModel
+{
+    private string _body = string.Empty;
+
+    [JsonPropertyName("body")]
+    public string Body { get => _body; set => _body = HttpUtility.UrlDecode(value, Encoding.UTF8); }
+
+    [JsonPropertyName("enc")]
+    public string Enc { get; set; } = string.Empty;
+}
