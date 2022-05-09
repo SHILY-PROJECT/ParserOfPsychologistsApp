3ce152ac187bd160ece023b5755aa17380026537
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-09 09:28:15 +0300

    -Удалён консольный UI

--- src/ParserOfPsychologists.ConsoleUI/Configuration/_usings.cs
+++
@@ -1,6 +0,0 @@
-﻿global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.Hosting;
-global using ParserOfPsychologists.Application.Models;
-global using ParserOfPsychologists.Application.Interfaces;
-global using ParserOfPsychologists.Application.Configuration;
-global using ParserOfPsychologists.ConsoleUI.Configuration;
