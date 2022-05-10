a0f0a6483e43e2234d3d0e5a80087113ac660c6d
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-10 14:41:41 +0300

    -Добавлен метод для извлечения числа из текстового поля бокса -Реализован метод 'OnCityChanged'

--- src/ParserOfPsychologists.WinFormsUI/Forms/MainForms.cs
+++ src/ParserOfPsychologists.WinFormsUI/Forms/MainForms.cs
@@ -2,6 +2,9 @@
 
 public partial class MainForms : Form
 {
+    private readonly string _fromLineStartsWith = "с";
+    private readonly string _toLineStartsWith = "по";
+
     private readonly IStateOfCityModule _stateOfCity;
     private readonly IParserWebRequestsFacade _facade;
 
@@ -32,16 +35,49 @@ public partial class MainForms : Form
     {
         this.citiesBox.SelectedValueChanged += async (s, e) =>
         {
-            if (s is ComboBox box && _stateOfCity.IsChanged(box.Text)) await _stateOfCity.ChangeCityAsync(box.Text);
+            if (s is ComboBox box && _stateOfCity.IsChanged(box.Text))
+                await _stateOfCity.ChangeCityAsync(box.Text);
+        };
+
+        this.parsePageFromBox.SelectedValueChanged += (s, e) =>
+        {
+            if (s is ComboBox fromBox && ExtractIntOf(fromBox.Text) is int from)
+            {
+                var textTo = this.parsePageToBox.Text;
+
+                this.parsePageToBox.Items.Clear();
+                this.parsePageToBox.Items.AddRange(CreateSequenceOfPages(_toLineStartsWith, from, _stateOfCity.NumberOfPagesAvailable - from + 1));
+
+                this.parsePageToBox.Text = from < ExtractIntOf(textTo) ? textTo : $"{_toLineStartsWith} {from}";
+            }
         };
     }
 
-    private void OnCityChanged(object obj, StateOfCityEventArgs args)
+    private void OnCityChanged(object? obj, StateOfCityEventArgs args)
     {
-        this.parsePageFromBox.Items.AddRange(Enumerable.Range(1, args.PagesAvailable).Select(x => $"с {x}").ToArray());
-        this.parsePageToBox.Items.AddRange(Enumerable.Range(1, args.PagesAvailable).Select(x => $"по {x}").ToArray());
+        this.parsePageFromBox.Items.Clear();
+        this.parsePageToBox.Items.Clear();
 
-        this.parsePageFromBox.SelectedIndex = 0;
-        this.parsePageToBox.SelectedIndex = this.parsePageFromBox.Items.Count - 1;
+        if (CreateSequenceOfPages(_fromLineStartsWith, 1, args.PagesAvailable) is string[] arrFrom && arrFrom.Any() &&
+            CreateSequenceOfPages(_toLineStartsWith, 1, args.PagesAvailable) is string[] arrTo && arrTo.Any())
+        {
+            this.parsePageFromBox.Items.AddRange(arrFrom);
+            this.parsePageToBox.Items.AddRange(arrTo);
+
+            this.parsePageToBox.Text = this.parsePageToBox.Items[this.parsePageToBox.Items.Count - 1] as string;
+            this.parsePageFromBox.Text = this.parsePageFromBox.Items[0] as string;
+        }
+    }
+
+    private static int ExtractIntOf(string text) => Convert.ToInt32(text.Split(' ').LastOrDefault());
+
+    private static string[] CreateSequenceOfPages(string lineStartsWith, int pageNumberingStartsWith, int pagesAvailable)
+    {
+        if (!lineStartsWith.All(x => char.IsLetter(x)))
+            throw new ArgumentException("String should consist of the letter.", nameof(lineStartsWith));
+
+        if (pageNumberingStartsWith == 0) return Array.Empty<string>();
+
+        return Enumerable.Range(pageNumberingStartsWith, pagesAvailable).Select(x => $"{lineStartsWith} {x}").ToArray();
     }
 }
