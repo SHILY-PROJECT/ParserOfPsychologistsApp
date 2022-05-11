5f43904ab29743bb5dc86334687850e35ff3379d
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-11 23:50:15 +0300

    -В настройках парсера добавлен эвент 'SettingsChanged' и метод его вызова 'OnSettingsChanged'

--- src/ParserOfPsychologists.Application/Models/ParserSettings.cs
+++ src/ParserOfPsychologists.Application/Models/ParserSettings.cs
@@ -2,10 +2,29 @@
 
 public class ParserSettings : IParserSettings
 {
-    public string CityOnInput { get; set; } = string.Empty;
-    
-    public int ToParsePagesFrom { get; set; }
-    public int ToParsePagesTo { get; set; }
+    private string _cityOnInput = string.Empty;
+    private int _pageFrom;
+    private int _pageTo;
+
+    public event EventHandler? SettingsChanged;
+
+    public string CityOnInput
+    {
+        get => _cityOnInput;
+        set { _cityOnInput = value; OnSettingsChanged(); }
+    }
+
+    public int PageFrom
+    {
+        get => _pageFrom;
+        set { _pageFrom = value; OnSettingsChanged(); }
+    }
+
+    public int PageTo
+    {
+        get => _pageTo;
+        set { _pageTo = value; OnSettingsChanged(); }
+    }
 
     public int TimeoutAfterRequestToOneNumberMainPageWithUsers { get; set; }
     public int TimeoutAfterRequestToOneUserPage { get; set; }
@@ -13,9 +32,16 @@ public class ParserSettings : IParserSettings
 
     public void SetTimeouts(string maskedText)
     {
-        var timeouts = Regex.Replace(maskedText, @"(сек.*?|\s)", "").Split('-').Select(x => (int)(double.Parse(x) * 1000)).ToArray();
+        var timeouts = Regex
+            .Replace(maskedText, @"(сек.*?|\s)", "").Split('-')
+            .Select(x => (int)(double.Parse(x) * 1000))
+            .ToArray();
+
         TimeoutAfterRequestToOneNumberMainPageWithUsers = timeouts[0];
         TimeoutAfterRequestToOneUserPage = timeouts[1];
         TimeoutAfterRequestToContactsOfOneUser = timeouts[2];
     }
+
+    protected void OnSettingsChanged() =>
+        SettingsChanged?.Invoke(this, EventArgs.Empty);
 }
