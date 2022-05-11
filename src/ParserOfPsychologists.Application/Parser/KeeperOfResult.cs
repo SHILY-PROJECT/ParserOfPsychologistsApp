4ebc5e2b1c85fb872a1976a73913248fbaad005d
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-12 00:16:55 +0300

    -Добавлен класс для сохранения результата

---
+++ src/ParserOfPsychologists.Application/Parser/KeeperOfResult.cs
@@ -0,0 +1,17 @@
+﻿namespace ParserOfPsychologists.Application.Parser;
+
+public class KeeperOfResult
+{
+    public static void SaveToFile(IEnumerable<UserData> users)
+    {
+        var file = GetFileOfResult(".csv");
+
+    }
+
+    private static FileInfo GetFileOfResult(string extension)
+    {
+        var file = new FileInfo(Path.Combine("results", $"result   {DateTime.Now:yyyy-MM-dd   HH-mm-ss---fffffff}{extension}"));
+        if (!file.Directory!.Exists) file.Directory.Create();
+        return file;
+    }
+}
