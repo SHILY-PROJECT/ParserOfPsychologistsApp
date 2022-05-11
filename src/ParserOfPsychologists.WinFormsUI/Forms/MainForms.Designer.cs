c1c8b273d18cea928f6d17c4534b245843943325
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-11 20:46:44 +0300

    -Добавлены UI элементы -Добавлены подписки на события

--- src/ParserOfPsychologists.WinFormsUI/Forms/MainForms.Designer.cs
+++ src/ParserOfPsychologists.WinFormsUI/Forms/MainForms.Designer.cs
@@ -15,128 +15,270 @@ partial class MainForms
 
     private void InitializeComponent()
     {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForms));
             this.citiesBox = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.parsePageToBox = new System.Windows.Forms.ComboBox();
             this.parsePageFromBox = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
+            this.startParsing = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.timeoutsBox = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.passInput = new System.Windows.Forms.TextBox();
+            this.loginInput = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // citiesBox
             // 
-            this.citiesBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.citiesBox.Font = new System.Drawing.Font("Segoe UI Semilight", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.citiesBox.ForeColor = System.Drawing.Color.Black;
             this.citiesBox.FormattingEnabled = true;
             this.citiesBox.Location = new System.Drawing.Point(6, 37);
+            this.citiesBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.citiesBox.Name = "citiesBox";
-            this.citiesBox.Size = new System.Drawing.Size(256, 23);
+            this.citiesBox.Size = new System.Drawing.Size(259, 23);
             this.citiesBox.TabIndex = 0;
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.parsePageToBox);
             this.groupBox1.Controls.Add(this.parsePageFromBox);
+            this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.startParsing);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.citiesBox);
-            this.groupBox1.Location = new System.Drawing.Point(12, 2);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI Semilight", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox1.Location = new System.Drawing.Point(7, 5);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(268, 355);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Size = new System.Drawing.Size(270, 188);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Поиск";
+            this.groupBox1.Text = "Настройки парсера";
             // 
             // parsePageToBox
             // 
-            this.parsePageToBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.parsePageToBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.parsePageToBox.Font = new System.Drawing.Font("Segoe UI Semilight", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.parsePageToBox.FormattingEnabled = true;
-            this.parsePageToBox.Location = new System.Drawing.Point(152, 81);
+            this.parsePageToBox.Location = new System.Drawing.Point(154, 84);
+            this.parsePageToBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.parsePageToBox.Name = "parsePageToBox";
             this.parsePageToBox.Size = new System.Drawing.Size(110, 23);
             this.parsePageToBox.TabIndex = 3;
             // 
             // parsePageFromBox
             // 
-            this.parsePageFromBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.parsePageFromBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.parsePageFromBox.Font = new System.Drawing.Font("Segoe UI Semilight", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.parsePageFromBox.FormattingEnabled = true;
-            this.parsePageFromBox.Location = new System.Drawing.Point(6, 81);
+            this.parsePageFromBox.Location = new System.Drawing.Point(7, 84);
+            this.parsePageFromBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.parsePageFromBox.Name = "parsePageFromBox";
             this.parsePageFromBox.Size = new System.Drawing.Size(110, 23);
             this.parsePageFromBox.TabIndex = 3;
             // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(220, 141);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 41);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(126, 84);
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label5.Location = new System.Drawing.Point(124, 84);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(19, 15);
+            this.label5.Size = new System.Drawing.Size(23, 19);
             this.label5.TabIndex = 2;
             this.label5.Text = "—";
+            // 
+            // button1
+            // 
+            this.startParsing.Font = new System.Drawing.Font("Segoe UI Semilight", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.startParsing.ForeColor = System.Drawing.Color.Black;
+            this.startParsing.Location = new System.Drawing.Point(6, 141);
+            this.startParsing.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.startParsing.Name = "button1";
+            this.startParsing.Size = new System.Drawing.Size(212, 41);
+            this.startParsing.TabIndex = 1;
+            this.startParsing.Text = "Начать парсинг";
+            this.startParsing.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 63);
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semilight", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(7, 66);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(188, 15);
+            this.label2.Size = new System.Drawing.Size(175, 15);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Номера страниц парсить (с - по)";
+            this.label2.Text = "Cтраницы для парсинга (с - по)";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 19);
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semilight", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(6, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 15);
+            this.label1.Size = new System.Drawing.Size(39, 15);
             this.label1.TabIndex = 2;
             this.label1.Text = "Город";
             // 
-            // button1
+            // timeoutsBox
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(6, 119);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(256, 30);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Парсить";
-            this.button1.UseVisualStyleBackColor = true;
+            this.timeoutsBox.Location = new System.Drawing.Point(6, 28);
+            this.timeoutsBox.Mask = "000.0 сек - 000.0 сек - 000.0 сек";
+            this.timeoutsBox.Name = "timeoutsBox";
+            this.timeoutsBox.Size = new System.Drawing.Size(266, 25);
+            this.timeoutsBox.TabIndex = 2;
+            this.timeoutsBox.Text = "003000300010";
+            this.timeoutsBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.timeoutsBox.TextMaskFormat = System.Windows.Forms.MaskFormat.IncludePromptAndLiterals;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.passInput);
+            this.groupBox2.Controls.Add(this.loginInput);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI Semilight", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox2.Location = new System.Drawing.Point(285, 5);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox2.Size = new System.Drawing.Size(279, 108);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Аккаунт";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkBox1.Location = new System.Drawing.Point(59, 79);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(161, 23);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Подключить аккаунт";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // passInput
+            // 
+            this.passInput.ForeColor = System.Drawing.Color.Black;
+            this.passInput.Location = new System.Drawing.Point(6, 51);
+            this.passInput.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.passInput.Name = "passInput";
+            this.passInput.PasswordChar = '*';
+            this.passInput.PlaceholderText = "Введите пароль...";
+            this.passInput.Size = new System.Drawing.Size(270, 25);
+            this.passInput.TabIndex = 0;
+            this.passInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // loginInput
+            // 
+            this.loginInput.ForeColor = System.Drawing.Color.Black;
+            this.loginInput.Location = new System.Drawing.Point(6, 22);
+            this.loginInput.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.loginInput.Name = "loginInput";
+            this.loginInput.PlaceholderText = "Введите логин...";
+            this.loginInput.Size = new System.Drawing.Size(270, 25);
+            this.loginInput.TabIndex = 0;
+            this.loginInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label7);
+            this.groupBox4.Controls.Add(this.timeoutsBox);
+            this.groupBox4.Font = new System.Drawing.Font("Segoe UI Semilight", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox4.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox4.Location = new System.Drawing.Point(285, 119);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox4.Size = new System.Drawing.Size(279, 74);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Настройки таймаутов между запросами";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semilight", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(6, 56);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(270, 15);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Главные страницы -> Пользователи -> Контакты";
             // 
             // MainForms
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(613, 370);
+            this.ClientSize = new System.Drawing.Size(574, 201);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox2);
+            this.Font = new System.Drawing.Font("Segoe UI Semilight", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
             this.Name = "MainForms";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "[WWW.B17.RU] PARSER OF PSYCHOLOGISTS";
+            this.Text = "[WWW.B17.RU] PARSER OF PSYCHOLOGISTS by SHILY";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
 
     }
 
     private ComboBox citiesBox;
     private GroupBox groupBox1;
-    private Button button1;
+    private Button startParsing;
     private ComboBox parsePageFromBox;
     private Label label2;
     private Label label1;
     private ComboBox parsePageToBox;
     private Label label5;
+    private Button button2;
+    private GroupBox groupBox2;
+    private TextBox loginInput;
+    private TextBox passInput;
+    private CheckBox checkBox1;
+    private GroupBox groupBox4;
+    private Label label7;
+    private MaskedTextBox timeoutsBox;
 }
--- src/ParserOfPsychologists.WinFormsUI/Forms/MainForms.cs
+++ src/ParserOfPsychologists.WinFormsUI/Forms/MainForms.cs
@@ -5,15 +5,13 @@ public partial class MainForms : Form
     private readonly string _fromLineStartsWith = "с";
     private readonly string _toLineStartsWith = "по";
 
-    private readonly ICityHandlerModule _stateOfCity;
     private readonly IParserWebRequestsFacade _facade;
+    private readonly IParserSettings _parserSettings;
 
-    public MainForms(IParserWebRequestsFacade facade, ICityHandlerModule stateOfCityModule)
+    public MainForms(IParserWebRequestsFacade facade, IParserSettings parserSettings)
     {
         _facade = facade;
-        _stateOfCity = stateOfCityModule;
-
-        _stateOfCity.CityChanged += OnCityChanged;
+        _parserSettings = parserSettings;
 
         InitializeComponent();
         RegisterFormEvents();
@@ -22,38 +20,70 @@ public partial class MainForms : Form
     private void RegisterFormEvents()
     {
         ActionOnEventsToLoadAndCloseForm();
-        ActionOnEventsChangeParserSettings();
+        ActionOnEventsParser();        
     }
 
     private void ActionOnEventsToLoadAndCloseForm()
     {
-        var cities = async () => (await _facade.FindCityAsync(string.Empty)).ToArray();
-        this.Load += async (s, e) => this.citiesBox.Items.AddRange(await cities.Invoke());
+        _facade.CityHandler.CityChanged += OnCityChanged;
+
+        this.Load += async (s, e) =>
+        {
+            var cities = async () => (await _facade.FindCityAsync(string.Empty)).ToArray();
+            this.citiesBox.Items.AddRange(await cities.Invoke());
+        };
     }
 
-    private void ActionOnEventsChangeParserSettings()
+    private void ActionOnEventsParser()
     {
         this.citiesBox.SelectedValueChanged += async (s, e) =>
         {
-            if (s is ComboBox box && _stateOfCity.IsChanged(box.Text))
-                await _stateOfCity.ChangeCityAsync(box.Text);
+            if (s is ComboBox box && _facade.CityHandler.IsChanged(_parserSettings.CityOnInput = box.Text))
+            {
+                await _facade.ChangeCityAsync();
+            }
         };
 
         this.parsePageFromBox.SelectedValueChanged += (s, e) =>
         {
-            if (s is ComboBox fromBox && ExtractIntOf(fromBox.Text) is int from)
+            if (s is ComboBox fromBox && PageNumberOf(fromBox.Text) is int from)
             {
                 var textTo = this.parsePageToBox.Text;
 
                 this.parsePageToBox.Items.Clear();
-                this.parsePageToBox.Items.AddRange(CreateSequenceOfPages(_toLineStartsWith, from, _stateOfCity.NumberOfPagesAvailable - from + 1));
+                this.parsePageToBox.Items.AddRange(CreateSequenceOfPages(_toLineStartsWith, from, _facade.CityHandler.NumberOfAvailablePagesForCurrentCity - from + 1));
 
-                this.parsePageToBox.Text = from < ExtractIntOf(textTo) ? textTo : $"{_toLineStartsWith} {from}";
+                this.parsePageToBox.Text = from < PageNumberOf(textTo) ? textTo : $"{_toLineStartsWith} {from}";
+            }
+        };
+
+        this.startParsing.Click += async (s, e) =>
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(this.citiesBox.Text))
+                    throw new InvalidOperationException("Select cities from the list.");
+
+                _parserSettings.SetTimeouts(this.timeoutsBox.Text);
+                _parserSettings.ToParsePagesTo = PageNumberOf(this.parsePageToBox.Text);
+                _parserSettings.ToParsePagesFrom = PageNumberOf(this.parsePageFromBox.Text);
+
+                await Parse();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ooops...", MessageBoxButtons.OK);
             }
         };
     }
 
-    private void OnCityChanged(object? obj, StateOfCityEventArgs args)
+    private async Task Parse()
+    {
+        var users = await _facade.ParseUsersByCityAsync();
+
+    }
+
+    private void OnCityChanged(object? obj, CityHandlerModuleEventArgs args)
     {
         this.parsePageFromBox.Items.Clear();
         this.parsePageToBox.Items.Clear();
@@ -69,7 +99,11 @@ public partial class MainForms : Form
         }
     }
 
-    private static int ExtractIntOf(string text) => Convert.ToInt32(text.Split(' ').LastOrDefault());
+    private static int PageNumberOf(string pageNumberWithPrefix)
+    {
+        if (int.TryParse(pageNumberWithPrefix.Split(' ').LastOrDefault(), out var value)) return value;
+        throw new ArgumentException("Failed to retrieve page number.", nameof(pageNumberWithPrefix));
+    }
 
     private static string[] CreateSequenceOfPages(string lineStartsWith, int pageNumberingStartsWith, int pagesAvailable)
     {
