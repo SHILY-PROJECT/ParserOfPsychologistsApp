404e75d3a6428be3f2348edc76094b0915db119d
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-12 23:45:35 +0300

    -Добавлено расширение для класса пользователя

---
+++ src/ParserOfPsychologists.Application/Toolkit/UserExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace ParserOfPsychologists.Application.Toolkit;
+
+public static class UserExtensions
+{
+    public static void ExtractSpecialtyAndCity(this UserModel user, string specialtyAndCity) =>
+        (user.Specialty, user.City) = specialtyAndCity.Replace("&nbsp;", "").Split('–') is string[] sc && sc.Length >= 2 ? (sc[0], sc[1]) : ("", "");
+}
