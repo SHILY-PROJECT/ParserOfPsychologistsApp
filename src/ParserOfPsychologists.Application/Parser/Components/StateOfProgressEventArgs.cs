2ecf8fbddc957bae103b254f44690a7f7ec0a12b
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-12 09:42:05 +0300

    -Добавлен кастомный класс аргументов события прогресса

---
+++ src/ParserOfPsychologists.Application/Parser/Components/StateOfProgressEventArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace ParserOfPsychologists.Application.Parser.Components;
+
+public class StateOfProgressEventArgs
+{
+    public StateOfProgressEventArgs(int numberOfPagesProcessed, int numberOfUsersProcessed)
+    {
+        NumberOfPagesProcessed = numberOfPagesProcessed;
+        NumberOfUsersProcessed = numberOfUsersProcessed;
+    }
+
+    public int NumberOfPagesProcessed { get; set; }
+    public int NumberOfUsersProcessed { get; set; }
+}
