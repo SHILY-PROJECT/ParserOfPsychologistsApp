d8406fb5ba0fed5de224f783bb3a4cc542eaa5c2
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-13 00:18:48 +0300

    -Добавлен метод для сохранения данных в таблицу

--- src/ParserOfPsychologists.Application/Parser/KeeperOfResult.cs
+++ src/ParserOfPsychologists.Application/Parser/KeeperOfResult.cs
@@ -1,16 +1,38 @@
-﻿namespace ParserOfPsychologists.Application.Parser;
+﻿using CsvHelper;
+
+namespace ParserOfPsychologists.Application.Parser;
 
 public class KeeperOfResult
 {
-    public static void SaveToFile(IEnumerable<UserData> users)
+    private static readonly string _dir = "results";
+
+    public static void OpenResultsFolder()
+    {
+        var dir = new DirectoryInfo(_dir);
+        if (!dir!.Exists) dir.Create();
+        Process.Start("explorer.exe", dir.FullName);
+    }
+
+    public static async Task SaveToFileAsync(IEnumerable<UserModel> users)
     {
         var file = GetFileOfResult(".csv");
 
+        using var streamWriter = new StreamWriter(file.FullName, true, Encoding.UTF8);
+        using var сsvWriter = new CsvWriter(streamWriter, System.Globalization.CultureInfo.InvariantCulture);
+        сsvWriter.Context.RegisterClassMap<UserModelClassMap>();
+
+        await сsvWriter.WriteRecordsAsync(users);
+
+        Process.Start(new ProcessStartInfo("explorer.exe", $@"/n, /select, {file.FullName}")
+        {
+            CreateNoWindow = true,
+            WindowStyle = ProcessWindowStyle.Normal
+        });
     }
 
     private static FileInfo GetFileOfResult(string extension)
     {
-        var file = new FileInfo(Path.Combine("results", $"result   {DateTime.Now:yyyy-MM-dd   HH-mm-ss---fffffff}{extension}"));
+        var file = new FileInfo(Path.Combine(_dir, $"result   {DateTime.Now:yyyy-MM-dd   HH-mm-ss---fffffff}{extension}"));
         if (!file.Directory!.Exists) file.Directory.Create();
         return file;
     }
