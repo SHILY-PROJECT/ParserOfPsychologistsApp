49a7409768ca04ccd48f83f1c1d729e40d6ea26a
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-13 23:13:34 +0300

    -Добавлены всякие методы и обёртки try catch

--- src/ParserOfPsychologists.Application/ApplicationFacade.cs
+++ src/ParserOfPsychologists.Application/ApplicationFacade.cs
@@ -6,23 +6,29 @@ public class ApplicationFacade : IApplicationFacade
     private readonly IParserSettings _parserSettings;
     private readonly ICityHandlerModule _cityHandler;
     private readonly IAccountManager _accountManager;
+    private readonly KeeperOfResult _keeperOfResult;
+
+    public event EventHandler<ApplicationInfoEventArgs>? ApplicationInfoSender;
 
     public ApplicationFacade(
         IParser parser,
         IParserSettings parserSettings,
         ICityHandlerModule cityHandlerModule,
-        IAccountManager accountManager)
+        IAccountManager accountManager,
+        KeeperOfResult keeperOfResult)
     {
         _parser = parser;
         _parserSettings = parserSettings;
         _cityHandler = cityHandlerModule;
         _accountManager = accountManager;
+        _keeperOfResult = keeperOfResult;
     }
 
+    public IParser Parser { get => _parser; }
     public IParserSettings ParserSettings { get => _parserSettings; }
     public ICityHandlerModule CityHandler { get => _cityHandler; }
 
-    public void OpenResultsFolder() => KeeperOfResult.OpenResultsFolder();
+    public void OpenResultsFolder() => _keeperOfResult.OpenResultsFolder();
 
     public async Task<bool> ConnectAnAccountAsync()
     {
@@ -39,18 +45,61 @@ public class ApplicationFacade : IApplicationFacade
 
     public async Task ChangeCityAsync()
     {
-        await _cityHandler.ChangeCityAsync(_parserSettings.CityOnInput);
+        try
+        {
+            await _cityHandler.ChangeCityAsync(_parserSettings.CityOnInput);
+        }
+        catch (Exception ex)
+        {
+            ApplicationInfoSender?.Invoke(this, new(ex.Message));
+        }
     }
 
-    public async Task<IReadOnlyCollection<string>> FindCityAsync(string cityName)
+    public async Task<IDictionary<string, string>> GetDefaultCities()
     {
-        return (await _cityHandler.FindCityAsync(cityName)).ToArray();
+        IDictionary<string, string> cities = null!;
+
+        try
+        {
+            cities = await _cityHandler.GetDefaultCities();
+        }
+        catch (Exception ex)
+        {
+            ApplicationInfoSender?.Invoke(this, new(ex.Message));
+        }
+     
+        return cities ?? new Dictionary<string, string>();
     }
 
-    public async Task<bool> ParseUsersByCityAsync()
+    public async Task<IDictionary<string, string>> FindCityAsync(string cityName)
     {
-        var users = (await _parser.ParseUsersByCityAsync()).ToList();
-        if (users.Any()) await KeeperOfResult.SaveToFileAsync(users);
-        return true;
+        IDictionary<string, string> cities = null!;
+
+        try
+        {
+            cities = await _cityHandler.FindCityAsync(cityName);
+        }
+        catch (Exception ex)
+        {
+            ApplicationInfoSender?.Invoke(this, new(ex.Message));
+        }
+        
+        return cities ?? new Dictionary<string, string>();
+    }
+
+    public async Task ParseUsersByCityAsync()
+    {
+        try
+        {
+            await _parser.ParseUsersByCityAsync();            
+        }
+        catch (Exception ex)
+        {
+            ApplicationInfoSender?.Invoke(this, new(ex.Message));
+        }
+        finally
+        {
+            await _keeperOfResult.SaveToFileAsync();
+        }
     }
 }
