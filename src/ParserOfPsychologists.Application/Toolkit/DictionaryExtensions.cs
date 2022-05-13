db6e944d6282f00fdfb03a18afc542ebb884d8ed
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-13 21:36:06 +0300

    -Добавлено расширение словаря

---
+++ src/ParserOfPsychologists.Application/Toolkit/DictionaryExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace ParserOfPsychologists.Application.Toolkit;
+
+public static class DictionaryExtensions
+{
+    public static void AddRange<TKey, TValue>(this IDictionary<TKey, TValue> source, IDictionary<TKey, TValue> provider)
+    {
+        foreach (var kv in provider)
+            source.Add(kv.Key, kv.Value);
+    }
+}
