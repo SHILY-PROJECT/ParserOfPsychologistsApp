b8dda15c2c902673af1d55fe7a0b786150adef57
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-13 23:17:23 +0300

    -Для расширения словаря добавлен метод 'TryAddRange'

--- src/ParserOfPsychologists.Application/Toolkit/DictionaryExtensions.cs
+++ src/ParserOfPsychologists.Application/Toolkit/DictionaryExtensions.cs
@@ -7,4 +7,10 @@ public static class DictionaryExtensions
         foreach (var kv in provider)
             source.Add(kv.Key, kv.Value);
     }
+
+    public static void TryAddRange<TKey, TValue>(this IDictionary<TKey, TValue> source, IDictionary<TKey, TValue> provider)
+    {
+        foreach (var kv in provider)
+            source.TryAdd(kv.Key, kv.Value);
+    }
 }
