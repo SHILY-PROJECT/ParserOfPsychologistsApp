044cecaf070cfa8a1b8a5de00cff5dadd5d6d959
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-14 00:27:21 +0300

    -Добавлены евент аргументы для приложения

---
+++ src/ParserOfPsychologists.Application/Parser/Components/ApplicationInfoEventArgs.cs
@@ -0,0 +1,11 @@
+﻿namespace ParserOfPsychologists.Application.Parser.Components;
+
+public class ApplicationInfoEventArgs : EventArgs
+{
+    public ApplicationInfoEventArgs() { }
+    public ApplicationInfoEventArgs(string message) => Message = message;
+
+    public string Title { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+    public Exception Ex { get; set; } = null!;
+}
