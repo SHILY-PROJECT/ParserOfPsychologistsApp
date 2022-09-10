7edb94b053e91b55676c7c4bbf5735774061c9d8
Ilya Shimbuev <iliyarock@gmail.com>
2022-09-10 22:34:57 +0300

    Removals and relocations

--- src/ParserOfPsychologists.Application/Models/AccountData.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ParserOfPsychologists.Application.Models;
-
-public class AccountData
-{
-    public string Login { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-}
