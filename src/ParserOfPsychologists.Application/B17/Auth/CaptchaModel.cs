ffa65bec25c4e3a48ba45afe44ac0e9a2a3731f4
Ilya Shimbuev <iliyarock@gmail.com>
2022-09-10 18:11:46 +0300

    Added models for transfer data: CaptchaModel, AccountModule

---
+++ src/ParserOfPsychologists.Application/B17/Auth/CaptchaModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace ParserOfPsychologists.Application.Models;
+
+public class CaptchaModel
+{
+    public string Id { get; set; } = string.Empty;
+    public string Key { get; set; } = string.Empty;
+    public Image Img { get; set; } = null!;
+    public string InputText { get; set; } = string.Empty;
+}
